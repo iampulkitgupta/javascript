192c5b7485d6f8a9468b97fb45ddadfb2b041d9b
iampulkitgupta <pulkit.noida@gmail.com>
2025-10-20 11:30:25 +0000

    operation2

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -39,3 +39,30 @@ let negValue = -value
 // console.log(typeof(negValue))
 
 
+// console.log(3%2)
+
+let str1 = "hello"
+let str2 = "pulkit"
+let str3 = str1 + " " + str2
+// console.log(str3)
+
+
+// console.log("1" + 2)
+// console.log(2 + 2 + "1")
+
+
+// console.log( ((3 + 4) * 5) % 3)
+
+// console.log(+true)
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+// console.log([num1, num2, num3])
+
+let gameCounter = 100
+++gameCounter;
+
+console.log(gameCounter)
+// console.log()
