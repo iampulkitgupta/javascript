6906884e38ed2bfd6492eb428fd652d6d87c808f
iampulkitgupta <pulkit.noida@gmail.com>
2025-10-20 09:39:54 +0000

    datatypes

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+//alert("hello") //we are using nodejs, not browser
+
+console.log(3 + 3) // code readability should be high
+
+console.log("Pulkit")
+
+let name = "Pulkit"
+let age = 30
+let isLoggedIn = true
+let state
+
+
+// number => 2 to power 53
+// bigint => 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not assigned
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
+
+
