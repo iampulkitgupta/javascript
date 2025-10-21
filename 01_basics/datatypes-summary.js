cbe9c323059770fa0bd33cd13a74d619a53b5744
iampulkitgupta <pulkit.noida@gmail.com>
2025-10-21 16:12:22 +0000

    Stack and Heap

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -36,3 +36,33 @@ const myFunc = function(){
 console.log(typeof(anotherId))
 
 //https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+//++++++++++++++++++++++++++++++++++
+
+//Stack (Primitive), Heap(Non-Primitive)
+
+let myName = "Pulkit"
+
+let mySecondName = myName
+
+mySecondName = "Advik"
+
+// console.log(myName)
+// console.log(mySecondName)
+
+
+let userOne = {
+    email: "pulkit@google.com",
+    upi: "user1@upi"
+}
+
+let userTwo = userOne
+
+// console.log(userOne)
+// console.log(userTwo)
+
+// userTwo.email = "hello@google.com"
+
+console.log(userOne)
+console.log(userTwo)
