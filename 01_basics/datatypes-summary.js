61b435c7f2702dab8a82f8c097f2ae3ac80633e7
iampulkitgupta <pulkit.noida@gmail.com>
2025-10-21 15:23:34 +0000

    datatype summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,38 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isUserLoggedIn = true
+const outsideTemp = null
+let userEmail
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+const bigNumber = 23464374567465476n
+
+//console.log(id === anotherId);
+
+// Reference(Non Primitive)
+
+//Array, Objects, Functions
+
+const heros = ["Superman", "spiderman", "Captain America"]
+
+let myObj = {
+    name: "Pulkit",
+    age: 22
+}
+
+//console.log(myObj)
+
+const myFunc = function(){
+    console.log("Hello World")
+}
+
+console.log(typeof(anotherId))
+
+//https://262.ecma-international.org/5.1/#sec-11.4.3
