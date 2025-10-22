8acf018e89a16ca3bd04ce90c85bcdd63ac67634
iampulkitgupta <pulkit.noida@gmail.com>
2025-10-22 01:07:26 +0000

    math and num

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,45 @@
+const score = 400
+// console.log(score)
+
+const balance = new Number(100)
+// console.log(balance)
+
+// console.log(balance.toFixed(2))
+
+
+const otherNumber = 1143.2353
+
+// console.log(otherNumber.toFixed(2))
+
+// console.log(otherNumber.toPrecision(3))
+
+
+
+const hundreds  = 10000000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+
+
+//++++++++++++++++Maths+++++++++++++++++++
+
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.51));
+// console.log(Math.ceil(4.41));
+// console.log(Math.floor(4.8))
+
+// console.log(Math.sqrt(25))
+
+// console.log(Math.pow(5,2))
+
+// console.log(Math.min(2,1,5,3))
+// console.log(Math.max(2,1,5,3))
+
+// console.log(Math.floor(Math.random()*10)+1)
+
+
+const min = 1
+const max = 6
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
