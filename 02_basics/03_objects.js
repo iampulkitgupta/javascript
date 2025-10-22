1f7f36e087cb6d3093436f3806dfb1a943f455bc
iampulkitgupta <pulkit.noida@gmail.com>
2025-10-22 08:42:58 +0000

    objects 1

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,36 @@
+//Objects
+
+//singleton
+//Object.create
+
+const mySym = Symbol("key1")
+
+const jsUser = {
+    name: "Pulkit",
+    "full name": "Pulkit Gupta", 
+    [mySym]: "mykey1",
+    age: 20,
+    location: "Delhi",
+    email: "pulkit@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+// console.log(jsUser.name)
+// console.log(jsUser["full name"])
+// console.log(jsUser.mySym)
+// console.log(jsUser[mySym])
+
+jsUser.email = "pulkit@openai.com"
+
+// console.log(jsUser)
+
+//Object.freeze(jsUser)
+jsUser.email = "pulkit@ms.com"
+// console.log(jsUser)
+
+jsUser.greeting = function(){
+    console.log("Hello Pulkit")
+}
+
+console.log(jsUser.greeting())
